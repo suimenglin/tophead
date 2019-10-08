07e4f50aab2ebe86605a696297d40bdf230fad6e
sml <sml@qq.com>
2019-10-08 20:09:01 +0800

    添加登录&主页路由

--- src/router.js
+++ src/router.js
@@ -1,11 +1,27 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import UseElement from './views/useElement'
+import Login from './views/login'
+import Home from './views/home'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      path: '/',
+      redirect: '/home'
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: Home
+    },
     {
       path: '/useElement',
       name: 'useElement',
