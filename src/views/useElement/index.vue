9fc889d210813c6eef30160b07678a9eb6b53db2
sml <sml@qq.com>
2019-10-08 19:46:11 +0800

    项目结构的搭建

---
+++ src/views/useElement/index.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="child">
+      <h2>这是element-ui测试</h2>
+      <h3>这是element - ui 单选框</h3>
+      <el-radio-group v-model="radio">
+        <el-radio :label="3">备选项</el-radio>
+        <el-radio :label="6">备选项</el-radio>
+        <el-radio :label="9">备选项</el-radio>
+      </el-radio-group>
+      <h3>
+          这是element-ui 的日期
+      </h3>
+      <div class="block">
+      <span class="demonstration">带快捷选项</span>
+      <el-date-picker
+        v-model="value2"
+        align="right"
+        type="date"
+        placeholder="选择日期"
+        :picker-options="pickerOptions">
+      </el-date-picker>
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      radio: 3,
+      pickerOptions: {
+        disabledDate (time) {
+          return time.getTime() > Date.now()
+        },
+        shortcuts: [{
+          text: '今天',
+          onClick (picker) {
+            picker.$emit('pick', new Date())
+          }
+        }, {
+          text: '昨天',
+          onClick (picker) {
+            const date = new Date()
+            date.setTime(date.getTime() - 3600 * 1000 * 24)
+            picker.$emit('pick', date)
+          }
+        }, {
+          text: '一周前',
+          onClick (picker) {
+            const date = new Date()
+            date.setTime(date.getTime() - 3600 * 1000 * 24 * 7)
+            picker.$emit('pick', date)
+          }
+        }]
+      },
+      value1: '',
+      value2: ''
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
