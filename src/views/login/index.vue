136515b84653e645f3d28af8b500a0dcee8e4f44
sml <sml@qq.com>
2019-10-09 11:40:17 +0800

    给登录按钮添加加载状态&添加用户阅读协议

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -14,17 +14,19 @@
               <el-input v-model="form.code" placeholder="请输入验证码"></el-input>
             </el-col>
             <el-col :span="9">
-              <el-button plain>发送验证码</el-button>
+              <el-button plain>
+                发送验证码
+              </el-button>
             </el-col>
           </el-row>
         </el-form-item>
-        <!-- <el-form-item>
-          <el-checkbox v-model="checked">
+        <el-form-item prop="read">
+          <el-checkbox v-model="form.read">
             我已阅读并同意<a href="#">用户协议</a>和<a href="#">隐私条款</a>
           </el-checkbox>
-        </el-form-item> -->
+        </el-form-item>
         <el-form-item>
-          <el-button type="primary" class="loginBtn" @click="login">登陆</el-button>
+          <el-button type="primary" class="loginBtn" @click="login" :loading="loginLoading">登陆</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -38,7 +40,8 @@ export default {
     return {
       form: {
         mobile: '13911111111',
-        code: '246810'
+        code: '246810',
+        read: false
       },
       rules: {
         mobile: [
@@ -48,8 +51,13 @@ export default {
         code: [
           { required: true, message: '请输入验证码', trigger: 'blur' },
           { min: 6, max: 6, message: '长度必须为6位', trigger: 'blur' }
+        ],
+        read: [
+          { required: true, message: '请先阅读用户协议', trigger: 'change' },
+          { pattern: /true/, message: '请先阅读用户协议', trigger: 'change' }
         ]
-      }
+      },
+      loginLoading: false
     }
   },
   methods: {
@@ -64,6 +72,7 @@ export default {
       })
     },
     submitData () {
+      this.loginLoading = true
       axios({
         url: 'http://ttapi.research.itcast.cn/mp/v1_0/authorizations',
         method: 'post',
@@ -77,6 +86,7 @@ export default {
       }).catch(err => {
         console.log(err)
         this.$message.error('手机号码或者验证码错误')
+        this.loginLoading = false
       })
     }
   }
